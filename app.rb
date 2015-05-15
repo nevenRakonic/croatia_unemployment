7357d54a5432c0eb4c6d6051f5f1a1c779f93462
NevenRakonic <neven.rakonic@gmail.com>
2015-05-15 18:29:18 +0200

    begin implementing d3.js

--- app.rb
+++ app.rb
@@ -3,14 +3,12 @@ require 'sinatra/activerecord'
 require 'json'
 require './models'
 
-# set :database_file, "config/database.yml"
-
 get '/' do
   erb :index
 end
 
 get '/.json' do
   content_type :json
-  UnemploymentData.all.to_json
+  UnemploymentData.all_data_with_unix_time.to_json
 end
 
